9cc099be9f11a41c0889d071f74bef6f901e7f87
Giovanni Costagliola <giovanni.costagliola@nexusat.it>
2020-11-08 11:41:22 +0100

    DayInfo.cs: added unit tests

---
+++ Nexusat.Utils.CalendarGenerator.Tests/DayInfoTests.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Nexusat.Utils.CalendarGenerator.Tests
+{
+    [TestClass]
+    public class DayInfoTests
+    {
+        [TestMethod]
+        public void DefaultCtor()
+        {
+            var di = new DayInfo();
+            
+            Assert.IsFalse(di.IsWorkingDay);
+            Assert.IsNull(di.Description);
+            Assert.IsNull(di.WorkingPeriods);
+        }
+
+        [TestMethod]
+        public void OnlyDescription()
+        {
+            var desc = "description";
+            var di = new DayInfo(desc);
+            
+            Assert.IsFalse(di.IsWorkingDay);
+            Assert.AreEqual(desc, di.Description);
+            Assert.IsNull(di.WorkingPeriods);
+        }
+        
+        [TestMethod]
+        public void EmptyWorkingPeriods()
+        {
+            // ReSharper disable once CollectionNeverUpdated.Local
+            var wps = new List<TimePeriod>();
+            var di = new DayInfo(workingPeriods:wps);
+            
+            Assert.IsFalse(di.IsWorkingDay);
+            Assert.IsNull(di.Description);
+            Assert.IsNull(di.WorkingPeriods);
+        }
+        
+        [TestMethod]
+        public void NonEmptyWorkingPeriods()
+        {
+            // ReSharper disable once CollectionNeverUpdated.Local
+            var wps = new List<TimePeriod>
+            {
+                new TimePeriod()
+            };
+            var di = new DayInfo(workingPeriods:wps);
+            
+            Assert.IsTrue(di.IsWorkingDay);
+            Assert.IsNull(di.Description);
+            Assert.IsNotNull(di.WorkingPeriods);
+        }
+    }
+}
