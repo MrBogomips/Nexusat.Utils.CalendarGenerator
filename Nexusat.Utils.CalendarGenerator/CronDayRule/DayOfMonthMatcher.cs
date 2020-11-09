16d32087f85661f3c80cd4d78c0b8710871be155
Giovanni Costagliola <giovanni.costagliola@nexusat.it>
2020-11-09 20:17:24 +0100

    Refactoring

--- Nexusat.Utils.CalendarGenerator/CronDayRule/DayOfMonthMatcher.cs
+++ Nexusat.Utils.CalendarGenerator/CronDayRule/DayOfMonthMatcher.cs
@@ -2,7 +2,6 @@ using System;
 
 namespace Nexusat.Utils.CalendarGenerator.CronDayRule
 {
-    // TODO: 
     public class DayOfMonthMatcher: IDateMatcher
     {
         public bool Match(DateTime date)
--- Nexusat.Utils.CalendarGenerator/CronDayRule/MonthMatcher.cs
+++ Nexusat.Utils.CalendarGenerator/CronDayRule/MonthMatcher.cs
@@ -2,7 +2,6 @@ using System;
 
 namespace Nexusat.Utils.CalendarGenerator.CronDayRule
 {
-    // TODO: 
     public class MonthMatcher: IDateMatcher
     {
         public bool Match(DateTime date)
